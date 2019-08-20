38df129a02c87d72c2c6f41b3c66ffaf133c7379
毛玉婷 <maoyuting02@meituan.com>
2019-08-20 15:50:36 +0800

    add(attention): seq2seq comment

--- 4-1.Seq2Seq/Seq2Seq-Tensor.py
+++ 4-1.Seq2Seq/Seq2Seq-Tensor.py
@@ -25,9 +25,12 @@ def make_batch(seq_data):
 
     for seq in seq_data:
         for i in range(2):
+            # 字符串乘法 'P'*3等于‘PPP’。将其进行文本补全，统一长度为5
             seq[i] = seq[i] + 'P' * (n_step - len(seq[i]))
 
         input = [num_dic[n] for n in seq[0]]
+        # 将output与target区分出来的原因？
+        # 这里对 input，output，target进行字符处理的原因：当输入有输错时，模糊处理，匹配到正确的output
         output = [num_dic[n] for n in ('S' + seq[1])]
         target = [num_dic[n] for n in (seq[1] + 'E')]
 
@@ -39,22 +42,31 @@ def make_batch(seq_data):
     return input_batch, output_batch, target_batch
 
 # Model
+# 编码-解码器模型，是有监督的学习模型 placeholder/variable/model /cost/optimzer
+# placehodler纬度解释： 元数据源数据量*每个数据字符长度*每个字符的特征向量长度n_class
 enc_input = tf.placeholder(tf.float32, [None, None, n_class]) # [batch_size, max_len(=encoder_step), n_class]
 dec_input = tf.placeholder(tf.float32, [None, None, n_class]) # [batch_size, max_len+1(=decoder_step) (becase of 'S' or 'E'), n_class]
 targets = tf.placeholder(tf.int64, [None, None]) # [batch_size, max_len+1], not one-hot
 
 with tf.variable_scope('encode'):
     enc_cell = tf.nn.rnn_cell.BasicRNNCell(n_hidden)
+    # dropoutWrapper cell,input_keep_prob输入保留率，为1时全部保留，output_keep_prob，输出保留率，为1时全部保留
     enc_cell = tf.nn.rnn_cell.DropoutWrapper(enc_cell, output_keep_prob=0.5)
     _, enc_states = tf.nn.dynamic_rnn(enc_cell, enc_input, dtype=tf.float32)
+    # 这里的state值指的是隐藏层状态
     # encoder state will go to decoder initial_state, enc_states : [batch_size, n_hidden(=128)]
 
 with tf.variable_scope('decode'):
     dec_cell = tf.nn.rnn_cell.BasicRNNCell(n_hidden)
     dec_cell = tf.nn.rnn_cell.DropoutWrapper(dec_cell, output_keep_prob=0.5)
+    # 这里与论文描述的不符，论文中 解码RNN的隐藏层状态是st（yt-1,st-1,c），每个隐藏层状态都要与c直接关联，而不是只有第一项关联c
+    # 这里的outputs也只是解码器的隐藏层
+    # dec_input ???
     outputs, _ = tf.nn.dynamic_rnn(dec_cell, dec_input, initial_state=enc_states, dtype=tf.float32)
     # outputs : [batch_size, max_len+1, n_hidden(=128)]
 
+# tf.layers.dense实现 activation（wx+b）https://www.w3cschool.cn/tensorflow_python/tensorflow_python-rn6a2tps.html
+# activation = none 线性激活
 model = tf.layers.dense(outputs, n_class, activation=None) # model : [batch_size, max_len+1, n_class]
 
 cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=model, labels=targets))
@@ -72,15 +84,23 @@ for epoch in range(5000):
 
 # Test
 def translate(word):
-    seq_data = [word, 'P' * len(word)]
+    #原论文中描述，原词和目标词长度不一定一样，因此修改源代码-1或者+1，试下效果
+    seq_data = [word, 'P' * (len(word)-1)]
 
     input_batch, output_batch, _ = make_batch([seq_data])
+    # argmax 返回向量或者矩阵中最大值的索引，0表示列，1表示行
+    # 输入的model纬度是 btach_size,max_len+1,n_calss ，即下面公式返回n_class 的最大索引
     prediction = tf.argmax(model, 2)
 
     result = sess.run(prediction, feed_dict={enc_input: input_batch, dec_input: output_batch})
+    # 查看result输出
+    print('result:')
+    print(result)
 
     decoded = [char_arr[i] for i in result[0]]
+    # 数组.index 查找E所在的位置
     end = decoded.index('E')
+    # 将数组转化为字符出啊
     translated = ''.join(decoded[:end])
 
     return translated.replace('P','')
